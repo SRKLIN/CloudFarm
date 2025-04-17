4d15681b1a575d1d14e11c4fddaf3aabbff1790f
JoselineTorres <joseline.torres64893@gmail.com>
2025-04-17 20:40:32 -0300

    Joseline: Ejemplo

--- src/almacen/cloudfarm/AlmacenCloudfarm.java
+++ src/almacen/cloudfarm/AlmacenCloudfarm.java
@@ -10,11 +10,10 @@ package almacen.cloudfarm;
  */
 public class AlmacenCloudfarm {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola, mundo!");
     }
-    
 }
+
+
